7325493907a97000a75be930038df68362d4274e
Sazzad <sazzad47.ju@gmail.com>
2023-08-13 19:42:21 +0600

    make it better

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -8,6 +8,6 @@ echo DATABASE_URL=$(aws ssm get-parameters --output text --region ap-south-1 --n
 echo PGDATABASE=$(aws ssm get-parameters --output text --region ap-south-1 --names PGDATABASE --with-decryption --query Parameters[1].Value) >> /home/ec2-user/bookstore/.env
 echo PGHOST=$(aws ssm get-parameters --output text --region ap-south-1 --names PGHOST --with-decryption --query Parameters[2].Value) >> /home/ec2-user/bookstore/.env
 echo PGPASSWORD=$(aws ssm get-parameters --output text --region ap-south-1 --names PGPASSWORD --with-decryption --query Parameters[3].Value) >> /home/ec2-user/bookstore/.env
-echo PGPORT=$(aws ssm get-parameters --output text --region ap-south-1 --names PGPORT --with-decryption --query Parameters[4].Value) >> /home/ec2-user/bookstore/.env
+echo PGPORT=$(aws ssm get-parameters --output text --region ap-south-1 --names PGPORT --with-decryption --query Parameters[4].Value | awk '{print int($0)}') >> /home/ec2-user/bookstore/.env
 echo PGUSER=$(aws ssm get-parameters --output text --region ap-south-1 --names PGUSER --with-decryption --query Parameters[5].Value) >> /home/ec2-user/bookstore/.env
 
