13478e027e9a79b9b91722c0204a1e6e754cd3c8
Sazzad <sazzad47.ju@gmail.com>
2023-08-16 20:14:10 +0600

    make it better

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -3,24 +3,24 @@ import { DataSource } from "typeorm";
 require('dotenv').config();
 
 export const dataSource = new DataSource({
-  type: "postgres",
-  url: process.env.DATABASE_URL,
-  host: process.env.PGHOST,
-  port: process.env.PGPORT ? parseInt(process.env.PGPORT) : undefined,
-  username: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
-  migrationsTableName: "migrations",
-  entities: [__dirname + '/entities/*.entity.{js,ts}'],
-  migrations: [__dirname + '/migrations/*{.ts,.js}'],
-  logging: true
+    type: "postgres",
+    url: process.env.DATABASE_URL,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT ? parseInt(process.env.PGPORT) : undefined,
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    migrationsTableName: "migrations",
+    entities: [__dirname + '/entities/*.entity.{js,ts}'],
+    migrations: [__dirname + '/migrations/*{.ts,.js}'],
+    logging: true
 });
 
 dataSource
-  .initialize()
-  .then(() => {
-    console.log("Data Source has been initialized!");
-  })
-  .catch((err) => {
-    console.error("Error during Data Source initialization", err);
-  });
+    .initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!");
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err);
+    });
